32dc6d20edeb1a53cd6edd735634cc7618e41741
penrose15 <seongju99@naver.com>
2022-12-07 14:22:45 +0900

    feat : add helloController and helloDto and test about helloController and HelloDto for practice

---
+++ springBoot-aws/src/test/java/com/example/springBootaws/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.example.springBootaws.dto;
+
+import com.example.springBootaws.web.HelloResponseDto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    void  helloResponseDtoTest() {
+        String name = "honggilDong";
+        int amount = 1000;
+
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        assertThat(helloResponseDto.getName()).isEqualTo("honggilDong");
+        assertThat(helloResponseDto.getAmount()).isEqualTo(1000);
+    }
+}
